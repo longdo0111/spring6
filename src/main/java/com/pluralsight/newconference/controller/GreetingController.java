4435ee6597b8227fff9f6c81b10fed14ddb691e4
Daniel <dohuynhlong@gmail.com>
2024-01-19 00:26:41 +1100

    implement thymeleaf

--- src/main/java/com/pluralsight/newconference/controller/GreetingController.java
+++ src/main/java/com/pluralsight/newconference/controller/GreetingController.java
@@ -14,4 +14,10 @@ public class GreetingController {
         return "greeting";
     }
 
+    @GetMapping("thyme")
+    public String thyme(Map<String, Object> model) {
+        model.put("message", "Hello Thymeleaf");
+        return "thyme";
+    }
+
 }
